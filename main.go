3df2d8c262659c97448dd8b32e3b7db0e5bb916c
Dave Arnold <dave@roknsound.com>
2024-06-14 14:59:06 -0700

    initial add

--- main.go
+++ main.go
@@ -1,13 +1,57 @@
-// main.go
 package main
 
 import (
-	provider "github.com/HappyPathway/terraform-provider-configstash"
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"context"
+
+	"github.com/google/go-github/v33/github"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"golang.org/x/oauth2"
 )
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"owner": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"repo": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"url": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"token": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+		ConfigureContextFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: d.Get("token").(string)},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client, err := github.NewEnterpriseClient(d.Get("url").(string), d.Get("url").(string), tc)
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+
+	return client, diags
+}
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: provider.New,
+		ProviderFunc: Provider,
 	})
 }
